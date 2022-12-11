5d9b3fc97fbc58118663495ea64f1e8419a72ef8
Binit Rupakheti <cruiserponder@protonmail.com>
2022-12-11 13:53:03 +0545

    feat: added basic routing using react-router-dom
    Added routing and simple routes for basic tags and categories of news.
    - fixed every element to have className instead of class
    - added underline for showing selection
    - the selection is also shown on the path of the page

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,42 @@
 import Home from './components/Home';
 import Login from './components/Login';
+import { Route, Routes } from 'react-router-dom';
+export const tabs = [
+	{
+		name: 'National',
+		slug: 'national',
+	},
+	{
+		name: 'Sports',
+		slug: 'sports',
+	},
+	{
+		name: 'Fashion',
+		slug: 'fashion',
+	},
+	{
+		name: 'Finance',
+		slug: 'finance',
+	},
+	{
+		name: 'Politics',
+		slug: 'politics',
+	},
+	{ name: 'Other News', slug: 'other-news' },
+];
 function App() {
-	return <Home />;
+	return (
+		<Routes>
+			<Route path='/' element={<Home selected='National' />} />;
+			{tabs.map(({ name, slug }) => (
+				<Route
+					key={name}
+					path={`/${slug}`}
+					element={<Home selected={name} />}
+				/>
+			))}
+		</Routes>
+	);
 }
 
 export default App;
