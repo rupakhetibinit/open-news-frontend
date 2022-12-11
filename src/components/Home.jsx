b3c54b97eb307aff359280cd41f5a889c1a2a295
Binit Rupakheti <cruiserponder@protonmail.com>
2022-12-11 11:22:47 +0545

    added tabs from headless-ui

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,7 +1,24 @@
 import React from 'react';
-
+import { Tab } from '@headlessui/react';
 const Home = () => {
-	return <div className='w-full h-screen'>Home</div>;
+	return (
+		<div className='w-full h-screen'>
+			<Tab.Group>
+				<Tab.List>
+					<Tab>National</Tab>
+					<Tab>International</Tab>
+					<Tab>Sports</Tab>
+					<Tab>Fashion</Tab>
+				</Tab.List>
+				<Tab.Panels>
+					<Tab.Panel>National News</Tab.Panel>
+					<Tab.Panel>International News</Tab.Panel>
+					<Tab.Panel>Sports News</Tab.Panel>
+					<Tab.Panel>Fashion News</Tab.Panel>
+				</Tab.Panels>
+			</Tab.Group>
+		</div>
+	);
 };
 
 export default Home;
