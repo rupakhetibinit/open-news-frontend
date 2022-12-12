3e96a44b117a1296b90018107e775df6474d24d1
Binit Rupakheti <cruiserponder@protonmail.com>
2022-12-12 20:53:35 +0545

    feat: react query implementation for basic queries

--- src/main.jsx
+++ src/main.jsx
@@ -4,11 +4,16 @@ import { BrowserRouter, Route } from 'react-router-dom';
 import Home from './components/Home';
 import './index.css';
 import App from './App';
-
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+const queryClient = new QueryClient();
 ReactDOM.createRoot(document.getElementById('root')).render(
 	<React.StrictMode>
 		<BrowserRouter>
-			<App />
+			<QueryClientProvider client={queryClient}>
+				<App />
+				<ReactQueryDevtools initialIsOpen />
+			</QueryClientProvider>
 		</BrowserRouter>
 	</React.StrictMode>
 );
