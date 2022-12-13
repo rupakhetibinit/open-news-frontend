89cee3f526bb9d5d7351b33a69b0651eed794ffc
Binit Rupakheti <cruiserponder@protonmail.com>
2022-12-13 13:34:09 +0545

    fix: added cards for articles and subarticles

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -6,13 +6,18 @@ import cx from 'classnames';
 import { useQuery } from '@tanstack/react-query';
 const Home = ({ selected }) => {
 	const navigate = useNavigate();
+	const [subArticles, setSubArticles] = useState([]);
 	const { data, error, isLoading } = useQuery({
 		queryKey: ['news'],
 		queryFn: async () => {
 			const resp = await axios.get('http://localhost:8000/api/article-list');
 			return resp.data;
 		},
-		onSuccess: (value) => console.log(value),
+		onSuccess: (value) => {
+			console.log(value);
+			const sub = value.slice(2);
+			setSubArticles([...sub]);
+		},
 	});
 	const index = selected == 'Other News' ? 1 : 0;
 	if (isLoading) return null;
@@ -66,195 +71,16 @@ const Home = ({ selected }) => {
 						</button>
 					</div>
 				</header>
-				<section className='text-gray-600 body-font'>
-					<div className='container px-5 py-4 mx-auto flex flex-wrap'>
-						<div className='flex w-full mb-5 flex-wrap'>
-							<h1 className='sm:text-3xl text-2xl font-medium title-font text-gray-900 lg:w-1/3 lg:mb-0 mb-4'>
-								{data[index].headline}
-							</h1>
-							<p className='lg:pl-6 lg:w-2/3 mx-auto leading-relaxed text-base'>
-								{data[index].introduction}
-							</p>
-						</div>
-						<div className='flex flex-wrap md:-m-2 -m-1'>
-							<div className='flex flex-wrap w-1/2'>
-								<div className='md:p-2 p-1 w-1/2'>
-									<img
-										alt='gallery'
-										className='w-full object-cover h-full object-center block'
-										src={'http://localhost:8000/' + data[index].image}
-									/>
-								</div>
-								<div className='md:p-2 p-1 w-1/2'>
-									<img
-										alt='gallery'
-										className='w-full object-cover h-full object-center block'
-										src='https://dummyimage.com/501x301'
-									/>
-								</div>
-								<div className='md:p-2 p-1 w-full'>
-									<img
-										alt='gallery'
-										className='w-full h-full object-cover object-center block'
-										src='https://dummyimage.com/600x360'
-									/>
-								</div>
-							</div>
-							<div className='flex flex-wrap w-1/2'>
-								<div className='md:p-2 p-1 w-full'>
-									<img
-										alt='gallery'
-										className='w-full h-full object-cover object-center block'
-										src='https://dummyimage.com/601x361'
-									/>
-								</div>
-								<div className='md:p-2 p-1 w-1/2'>
-									<img
-										alt='gallery'
-										className='w-full object-cover h-full object-center block'
-										src='https://dummyimage.com/502x302'
-									/>
-								</div>
-								<div className='md:p-2 p-1 w-1/2'>
-									<img
-										alt='gallery'
-										className='w-full object-cover h-full object-center block'
-										src='https://dummyimage.com/503x303'
-									/>
-								</div>
-							</div>
-						</div>
-					</div>
+				<section className='text-gray-600 flex flex-row body-font justify-center'>
+					<ArticleCard {...data[0]} />
+					<ArticleCard {...data[1]} />
 				</section>
 				<section className='text-gray-600 body-font'>
 					<div className='container px-5 py-4 mx-auto'>
 						<div className='flex flex-wrap -m-4'>
-							<div className='lg:w-1/3 sm:w-1/2 p-4'>
-								<div className='flex relative'>
-									<img
-										alt='gallery'
-										className='absolute inset-0 w-full h-full object-cover object-center'
-										src='https://dummyimage.com/600x360'
-									/>
-									<div className='px-8 py-10 relative z-10 w-full border-4 border-gray-200 bg-white opacity-0 hover:opacity-100'>
-										<h2 className='tracking-widest text-sm title-font font-medium text-indigo-500 mb-1'>
-											THE SUBTITLE
-										</h2>
-										<h1 className='title-font text-lg font-medium text-gray-900 mb-3'>
-											Shooting Stars
-										</h1>
-										<p className='leading-relaxed'>
-											Photo booth fam kinfolk cold-pressed sriracha leggings
-											jianbing microdosing tousled waistcoat.
-										</p>
-									</div>
-								</div>
-							</div>
-							<div className='lg:w-1/3 sm:w-1/2 p-4'>
-								<div className='flex relative'>
-									<img
-										alt='gallery'
-										className='absolute inset-0 w-full h-full object-cover object-center'
-										src='https://dummyimage.com/601x361'
-									/>
-									<div className='px-8 py-10 relative z-10 w-full border-4 border-gray-200 bg-white opacity-0 hover:opacity-100'>
-										<h2 className='tracking-widest text-sm title-font font-medium text-indigo-500 mb-1'>
-											THE SUBTITLE
-										</h2>
-										<h1 className='title-font text-lg font-medium text-gray-900 mb-3'>
-											The Catalyzer
-										</h1>
-										<p className='leading-relaxed'>
-											Photo booth fam kinfolk cold-pressed sriracha leggings
-											jianbing microdosing tousled waistcoat.
-										</p>
-									</div>
-								</div>
-							</div>
-							<div className='lg:w-1/3 sm:w-1/2 p-4'>
-								<div className='flex relative'>
-									<img
-										alt='gallery'
-										className='absolute inset-0 w-full h-full object-cover object-center'
-										src='https://dummyimage.com/603x363'
-									/>
-									<div className='px-8 py-10 relative z-10 w-full border-4 border-gray-200 bg-white opacity-0 hover:opacity-100'>
-										<h2 className='tracking-widest text-sm title-font font-medium text-indigo-500 mb-1'>
-											THE SUBTITLE
-										</h2>
-										<h1 className='title-font text-lg font-medium text-gray-900 mb-3'>
-											The 400 Blows
-										</h1>
-										<p className='leading-relaxed'>
-											Photo booth fam kinfolk cold-pressed sriracha leggings
-											jianbing microdosing tousled waistcoat.
-										</p>
-									</div>
-								</div>
-							</div>
-							<div className='lg:w-1/3 sm:w-1/2 p-4'>
-								<div className='flex relative'>
-									<img
-										alt='gallery'
-										className='absolute inset-0 w-full h-full object-cover object-center'
-										src='https://dummyimage.com/602x362'
-									/>
-									<div className='px-8 py-10 relative z-10 w-full border-4 border-gray-200 bg-white opacity-0 hover:opacity-100'>
-										<h2 className='tracking-widest text-sm title-font font-medium text-indigo-500 mb-1'>
-											THE SUBTITLE
-										</h2>
-										<h1 className='title-font text-lg font-medium text-gray-900 mb-3'>
-											Neptune
-										</h1>
-										<p className='leading-relaxed'>
-											Photo booth fam kinfolk cold-pressed sriracha leggings
-											jianbing microdosing tousled waistcoat.
-										</p>
-									</div>
-								</div>
-							</div>
-							<div className='lg:w-1/3 sm:w-1/2 p-4'>
-								<div className='flex relative'>
-									<img
-										alt='gallery'
-										className='absolute inset-0 w-full h-full object-cover object-center'
-										src='https://dummyimage.com/605x365'
-									/>
-									<div className='px-8 py-10 relative z-10 w-full border-4 border-gray-200 bg-white opacity-0 hover:opacity-100'>
-										<h2 className='tracking-widest text-sm title-font font-medium text-indigo-500 mb-1'>
-											THE SUBTITLE
-										</h2>
-										<h1 className='title-font text-lg font-medium text-gray-900 mb-3'>
-											Holden Caulfield
-										</h1>
-										<p className='leading-relaxed'>
-											Photo booth fam kinfolk cold-pressed sriracha leggings
-											jianbing microdosing tousled waistcoat.
-										</p>
-									</div>
-								</div>
-							</div>
-							<div className='lg:w-1/3 sm:w-1/2 p-4'>
-								<div className='flex relative'>
-									<img
-										alt='gallery'
-										className='absolute inset-0 w-full h-full object-cover object-center'
-										src='https://dummyimage.com/606x366'
-									/>
-									<div className='px-8 py-10 relative z-10 w-full border-4 border-gray-200 bg-white opacity-0 hover:opacity-100'>
-										<h2 className='tracking-widest text-sm title-font font-medium text-indigo-500 mb-1'>
-											THE SUBTITLE
-										</h2>
-										<h1 className='title-font text-lg font-medium text-gray-900 mb-3'>
-											Alper Kamu
-										</h1>
-										<p className='leading-relaxed'>
-											Photo booth fam kinfolk cold-pressed sriracha leggings
-											jianbing microdosing tousled waistcoat.
-										</p>
-									</div>
-								</div>
-							</div>
+							{subArticles.map((i) => (
+								<SubArticlesCard {...subArticles[i]} />
+							))}
 						</div>
 					</div>
 				</section>
@@ -264,3 +90,65 @@ const Home = ({ selected }) => {
 };
 
 export default Home;
+
+const ArticleCard = ({ image, introduction, headline, byline }) => {
+	return (
+		<div className='px-4 py-8 max-w-xl hover:cursor-pointer'>
+			<div className='bg-white shadow-2xl rounded-lg mb-6 tracking-wide'>
+				<div className='md:flex-shrink-0'>
+					<img
+						src={image}
+						alt='mountains'
+						className='w-[40vw] h-96 rounded-lg rounded-b-none object-cover'
+					/>
+				</div>
+				<div className='px-4 py-2 mt-2'>
+					<h2 className='font-bold text-2xl text-gray-800 tracking-normal'>
+						{headline}
+					</h2>
+					<p className='text-sm text-gray-700 px-2 mr-1'>{introduction}</p>
+					<div className='flex items-center justify-between mt-2 mx-6'>
+						<a href='#' className='text-blue-500 text-xs -ml-3 '>
+							Show More
+						</a>
+					</div>
+					<div className='author flex items-center -ml-3 my-3'>
+						<div className='user-logo'>
+							<img
+								className='w-12 h-12 object-cover rounded-full mx-4  shadow'
+								src='https://images.unsplash.com/photo-1492562080023-ab3db95bfbce?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=731&q=80'
+								alt='avatar'
+							/>
+						</div>
+						<h2 className='text-sm tracking-tighter text-gray-900'>
+							<a>{byline}</a>{' '}
+						</h2>
+					</div>
+				</div>
+			</div>
+		</div>
+	);
+};
+
+const SubArticlesCard = ({ headline, byline, introduction, image }) => {
+	return (
+		<div className='lg:w-1/3 sm:w-1/2 p-4'>
+			<div className='flex relative'>
+				<img
+					alt='gallery'
+					className='absolute inset-0 w-full h-full object-cover object-center'
+					src={image}
+				/>
+				<div className='px-8 py-10 relative z-10 w-full border-4 border-gray-200 bg-white opacity-0 hover:opacity-100'>
+					<h2 className='tracking-widest text-sm title-font font-medium text-indigo-500 mb-1'>
+						{byline}
+					</h2>
+					<h1 className='title-font text-lg font-medium text-gray-900 mb-3'>
+						{headline}
+					</h1>
+					<p className='leading-relaxed'>{introduction}</p>
+				</div>
+			</div>
+		</div>
+	);
+};
